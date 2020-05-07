3a96ea49c6115864b5a754b9f889ea50af7ffac6
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-07 21:17:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,33 +14,8 @@ class Shoe
   end
   
   def brand=(brand)
-    @brand = brand
+    @brand = (brand)
     BRANDS << brand
-  end
-
-end
-
-
-
-
-
-class Book
-  attr_accessor :author, :page_count
-  attr_reader :title, :genre
-
-  GENRES = []
-
-  def initialize(title)
-    @title = title
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-  
-  def genre=(genre)
-    @genre = (genre)
-    GENRES << genre
   end
 
 end
